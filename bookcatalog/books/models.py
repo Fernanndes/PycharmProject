d80146e5ac4511990d9b7f042a9c83266f35d78b
Fernanndes <fernanndes@edu.unisinos.br>
2024-06-28 22:59:05 -0300

    added images from urls to models

--- bookcatalog/books/models.py
+++ bookcatalog/books/models.py
@@ -6,6 +6,7 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     isbn = models.CharField(max_length=13)
+    cover_image_url = models.URLField(max_length=200, null=True, blank=True)
     summary = models.TextField()
 
     def __str__(self):
